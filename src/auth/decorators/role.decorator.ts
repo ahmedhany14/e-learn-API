aaad54b4a159efd58d97644a75c42de62379ba34
Ahmed Hany <ah7608867@gmail.com>
2025-01-27 12:24:16 +0200

    fix[server]: fix roles to make api have multi roles

--- src/auth/decorators/role.decorator.ts
+++ src/auth/decorators/role.decorator.ts
@@ -2,5 +2,5 @@ import { SetMetadata } from '@nestjs/common';
 import { RoleEnum } from '../enums/role.enum';
 import { ROLE_TYPE_KEY } from '../../common/constants/role.constants';
 
-export const ROLE = (roleType: RoleEnum) =>
+export const ROLE = (...roleType: RoleEnum[]) =>
   SetMetadata(ROLE_TYPE_KEY, roleType);
