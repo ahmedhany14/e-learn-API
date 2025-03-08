1e9f2cc05fd1e6430974e8e11619be3504ba413a
ahmed_hany <ah7608867@gmail.com>
2025-03-08 06:12:46 +0200

    feat[server]: add course, section and video enums

---
+++ src/courses/entities/enums/course.enums.ts
@@ -0,0 +1,16 @@
+export enum CourseEnum {
+    ID = 'id',
+    IMAGE_URL = 'image_url',
+    TITLE = 'title',
+    DESCRIPTION = 'description',
+    REQUIREMENTS = 'requirements',
+    WHAT_YOU_LEARN = 'what_you_learn',
+    STATE = 'state',
+    PRICE = 'price',
+    CREATED_AT = 'created_at',
+    UPDATED_AT = 'updated_at',
+}
+
+export enum CourseRelations{
+    INSTRUCTOR = 'instructor',
+}
---
+++ src/courses/entities/enums/videos.enums.ts
@@ -0,0 +1,10 @@
+export enum VideoEnum {
+    ID = 'id',
+    TITLE = 'title',
+    VIDEO_URL = 'video_url',
+    ORDER = 'order',
+}
+
+export enum VideoRelations {
+    SECTION = 'section',
+}
