e2608093fd011b71671fd6a94f273785771e334f
ahmed_hany <ah7608867@gmail.com>
2025-03-15 04:14:56 +0200

    refactor[server]: remove factory and move logic to enroll module

--- src/payments/enroll-courses/services/enroll-courses.service.ts
+++ src/payments/enroll-courses/services/enroll-courses.service.ts
@@ -1,15 +1,98 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { BadRequestException, Inject, Injectable, Logger, NotFoundException } from '@nestjs/common';
+
+// repository
 import { EntollCoursesRepo } from '../repository/entoll-courses.repo';
+
+// services
+import { StripeService } from 'src/payments/stripe/stripe.service';
+import { PaymentsService } from 'src/payments/payments.service';
+import { CourseService } from 'src/courses/service/course.service';
+import { ViasPaymentDataDto } from '../dto/payment.data.dto';
+import { CourseEnum } from 'src/courses/entities/course.enums';
+import { CourseStatusEnum } from 'src/courses/enums/course.status.enum';
+
+// enums and dtos
+
 
 @Injectable()
 export class EnrollCoursesService {
+    private readonly logger = new Logger(EnrollCoursesService.name);
+
     constructor(
         @Inject()
-        private readonly entollCoursesRepo: EntollCoursesRepo
+        private readonly entollCoursesRepo: EntollCoursesRepo,
+
+        @Inject()
+        private readonly CourseService: CourseService,
+
+        @Inject()
+        private readonly stripeService: StripeService,
+
+        @Inject()
+        private readonly paymentsService: PaymentsService,
+
     ) { }
 
     createNewEnrolledCourse(account_id: number, course_id: number) {
         return this.entollCoursesRepo.createNewEnrolledCourse(account_id, course_id);
+    }
+
+
+    async enrollCoursesByVisa(course_id: number, visaPaymentDataDto: ViasPaymentDataDto, account_id: number) {
+        try {
+            // check if course exists
+
+            const course = await this.CourseService.getCourse(
+                [CourseEnum.ID, CourseEnum.PRICE, CourseEnum.STATE],
+                [],
+                course_id
+            );
+
+            if (!course || course.state !== CourseStatusEnum.PUBLISHED) {
+                throw new NotFoundException({
+                    message: 'Process payment failed, course not found or not published',
+                });
+            }
+
+            // process payment
+            const paymentIntent = await this.stripeService.processPayment(
+                course.price,
+                visaPaymentDataDto.paymentMethodId
+            );
+
+            if (paymentIntent.status !== 'succeeded') {
+                throw new BadRequestException({
+                    message: 'Process payment failed',
+                    details: 'Payment not succeeded',
+                });
+            }
+
+            // add course to account as enrolled course
+
+            const enrolled_course = await this.createNewEnrolledCourse(
+                account_id, course_id
+            );
+            await this.paymentsService.createPaymentHistory(
+                {
+                    amount: course.price,
+                    country: visaPaymentDataDto.country,
+                    currency: 'usd',
+                    status: 'success',
+                },
+                account_id
+            );
+
+        }
+        catch (e) {
+            throw new BadRequestException({
+                message: 'Process payment failed',
+                details: e.message,
+            });
+        } finally {
+            this.logger.log('Payment process completed');
+        }
+        // create PaymentHistory with stripe, and save it
+
     }
 
 }
--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -4,7 +4,6 @@ import { EnrollCoursesModule } from './enroll-courses/enroll-courses.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PaymentsHistory } from './entities/payments.history.entity';
 
-import { FacadePatternProcessPaymentsService } from './patterns/facade.pattern.process.payments.service';
 import { PaymentsService } from './payments.service';
 import { PaymentsRepositoyService } from './payments.repositoy.service';
 import { CoursesModule } from 'src/courses/courses.module';
@@ -18,7 +17,7 @@ import { CoursesModule } from 'src/courses/courses.module';
         forwardRef(() => EnrollCoursesModule),
         CoursesModule
     ],
-    providers: [FacadePatternProcessPaymentsService, PaymentsService, PaymentsRepositoyService],
-    exports: [FacadePatternProcessPaymentsService]
+    providers: [PaymentsService, PaymentsRepositoyService],
+    exports: [PaymentsService]
 })
 export class PaymentsModule { }
