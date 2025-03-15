260449fae7fec0c5caf66cc905ea47b2159280ca
ahmed_hany <ah7608867@gmail.com>
2025-03-15 06:36:59 +0200

    feat[server]: add base monitor payments, to check if there is a refund from the bank or not

--- src/payments/schedulers/chargeback-monitor.service.ts
+++ src/payments/schedulers/chargeback-monitor.service.ts
@@ -6,7 +6,7 @@ export class ChargebackMonitorService {
     private readonly logger = new Logger(ChargebackMonitorService.name);
 
     @Cron(CronExpression.EVERY_DAY_AT_1AM)
-    logSomething() {
+    async monitorPayments() {
         this.logger.debug('Chargeback monitor service is being executed...');
         /*
         Will implement the logic to check for chargebacks and update the payment status
