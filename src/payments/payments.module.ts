feb686fd058da19ea8b3f18e7acdc3d3043c90d3
ahmed_hany <ah7608867@gmail.com>
2025-03-15 06:39:29 +0200

    refactor[server]: move all payment modules in one dir

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -1,6 +1,6 @@
 import { Module, forwardRef } from '@nestjs/common';
-import { StripeModule } from './stripe/stripe.module';
-import { EnrollCoursesModule } from './enroll-courses/enroll-courses.module';
+import { StripeModule } from './modules/stripe/stripe.module';
+import { EnrollCoursesModule } from './modules/enroll-courses/enroll-courses.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PaymentsHistory } from './entities/payments.history.entity';
 
