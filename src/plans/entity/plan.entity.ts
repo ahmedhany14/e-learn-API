753cad2c6d28507c684311e58c3b73735f79fd08
ahmed_hany <ah7608867@gmail.com>
2025-04-02 19:43:39 +0200

    fix[server]: fix plan entity

--- src/plans/entity/plan.entity.ts
+++ src/plans/entity/plan.entity.ts
@@ -1,79 +1,72 @@
-import {
-  Entity,
-  PrimaryGeneratedColumn,
-  Column,
-  ManyToOne,
-  JoinColumn,
-  OneToMany,
-} from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn, OneToMany } from 'typeorm';
 import { Account } from '../../account/entity/account.entity';
 import { Plan_Account } from '../../account/entity/account.plan.entity';
 
 @Entity('plans')
 export class Plan {
-  @PrimaryGeneratedColumn()
-  id: number;
+    @PrimaryGeneratedColumn()
+    id: number;
 
-  @Column({
-    type: 'varchar',
-    length: 16,
-    nullable: false,
-    unique: true,
-  })
-  plan_name: string;
+    @Column({
+        type: 'varchar',
+        length: 16,
+        nullable: false,
+        unique: true,
+    })
+    plan_name: string;
 
-  @Column({
-    type: 'int',
-    nullable: false,
-    default: 0,
-  })
-  plan_price: number;
+    @Column({
+        type: 'int',
+        nullable: false,
+        default: 0,
+    })
+    plan_price: number;
 
-  @Column({
-    type: 'int',
-    nullable: false,
-    default: 0,
-  })
-  plan_duration: number;
+    @Column({
+        type: 'int',
+        nullable: false,
+        default: 0,
+    })
+    plan_duration: number;
 
-  @Column({
-    type: 'boolean',
-    nullable: false,
-    default: true,
-  })
-  is_active: boolean;
+    @Column({
+        type: 'boolean',
+        nullable: false,
+        default: true,
+    })
+    is_active: boolean;
 
-  @Column({
-    type: 'timestamp with time zone',
-    default: () => 'CURRENT_TIMESTAMP',
-  })
-  created_at: Date;
+    @Column({
+        type: 'timestamp with time zone',
+        default: () => 'CURRENT_TIMESTAMP',
+    })
+    created_at: Date;
 
-  @Column({
-    type: 'timestamp with time zone',
-    default: () => 'CURRENT_TIMESTAMP',
-    onUpdate: 'CURRENT_TIMESTAMP',
-  })
-  updated_at: Date;
+    @Column({
+        type: 'timestamp with time zone',
+        default: () => 'CURRENT_TIMESTAMP',
+        onUpdate: 'CURRENT_TIMESTAMP',
+    })
+    updated_at: Date;
 
-  @OneToMany(() => Plan_Account, (plan_account) => plan_account.plan, {
-    lazy: true,
-  })
-  plans_account: Promise<Plan_Account[]>;
+    @OneToMany(() => Plan_Account, (plan_account) => plan_account.plan, {
+        lazy: true,
+    })
+    plans_account: Promise<Plan_Account[]>;
 
-  // each plan is created by an admin, and multiple plans can be created by the same admin
-  @ManyToOne(() => Account, (account) => account.plans, {
-    //eager: true,
-    nullable: false,
-  })
-  @JoinColumn({ name: 'admin_id' })
-  admin_id: Account;
+    // each plan is created by an admin, and multiple plans can be created by the same admin
+    @ManyToOne(() => Account, (account) => account.plans, {
+        //eager: true,
+        nullable: false,
+    })
+    @JoinColumn({ name: 'admin_id' })
+    admin_id: Account;
 
-  // each plan can be updated by an admin, and multiple plans can be updated by the same admin
-  @ManyToOne(() => Account, (account) => account.plans_updated, {
-    //eager: true,
-    nullable: false,
-  })
-  @JoinColumn({ name: 'updated_by' })
-  updated_by: Account;
+    // each plan can be updated by an admin, and multiple plans can be updated by the same admin
+    @ManyToOne(() => Account, (account) => account.plans_updated, {
+        //eager: true,
+        // nullable: false,
+    })
+    @JoinColumn({ name: 'updated_by' })
+    updated_by: Account;
 }
