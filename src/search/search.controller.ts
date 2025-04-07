08066c1f7225c8071fd65d9a1140e409f63ddfd2
ahmed_hany <ah7608867@gmail.com>
2025-04-07 04:56:05 +0200

    feat[server]: add apis

--- src/search/search.controller.ts
+++ src/search/search.controller.ts
@@ -1,7 +1,37 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AUTH } from '../auth/decorators/auth.decorator';
 import { AuthEnum } from '../auth/enums/auth.enum';
 
 @AUTH(AuthEnum.BEARER)
 @Controller('search')
-export class SearchController {}
+export class SearchController {
+    @Get('/:search_text')
+    async getCourseBySearchText(
+        @Param('search_text') searchText: string,
+        @Query('rating') rating: number,
+    ) {
+        return `courses with search text ${searchText} and rating more than or equal ${rating}`;
+    }
+
+    @Get('explore/topic/:tag')
+    async getCourseWithTopic(@Param('tag') tag: string, @Query('rating') rating: number) {
+        return `courses with topic ${tag} and rating more than or equal ${rating}`;
+    }
+
+    @Get('explore/category/:category')
+    async getCourseWithCategory(
+        @Param('category') category: string,
+        @Query('rating') rating: number,
+    ) {
+        return `courses with category ${category} and rating more than or equal ${rating}`;
+    }
+
+    @Get('explore/:category/:subcategory')
+    async getCourseWithSubCategory(
+        @Param('category') category: string,
+        @Param('subcategory') subcategory: string,
+        @Query('rating') rating: number,
+    ) {
+        return `courses with category ${category}, subcategory ${subcategory} and rating more than or equal ${rating}`;
+    }
+}
